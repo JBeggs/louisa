6fac62eb3d33faceb3fdfeb41404b040c3c33723
JBeggs <django.python.pro@gmail.com>
2026-05-08 12:35:25 +0200

    linter fixes

--- src/components/cart/CartFulfilmentBlock.tsx
+++ src/components/cart/CartFulfilmentBlock.tsx
@@ -33,7 +33,7 @@ export default function CartFulfilmentBlock({ onCartUpdate }: CartFulfilmentBloc
   const mode = useSyncExternalStore(
     subscribeStoreFulfilmentMode,
     getStoreFulfilmentMode,
-    () => 'delivery',
+    (): StoreFulfilmentMode => 'delivery',
   )
 
   useEffect(() => {
